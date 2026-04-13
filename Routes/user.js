5d8ded0472eb894ee8c773b557718f4b8d0bba62
satywan kumar <satywankumar646@gmail.com>
2026-04-13 08:05:29 +0530

    signup api complete

--- Routes/user.js
+++ Routes/user.js
@@ -1,18 +1,48 @@
 const express = require('express')
 const Router = express.Router()
+const User = require('../models/User')
+const bcrypt = require('bcrypt')
 
 // signup api
-Router.post('/signup',(req,res)=>{
-    res.status(200).json({
-        msg:'signup response'
-    })
+Router.post('/signup',async(req,res)=>{
+    try
+    {
+        console.log(req.body)
+        const user = await User.find({email:req.body.email})
+        if(user.length > 0)
+        {
+           return  res.status(200).json({
+            error:'email already registered...'
+           })
+        }
+        const hash = await bcrypt.hash(req.body.password,10)
+        console.log(hash)
+        const newUser = new User({
+            fullName:req.body.fullName,
+            email:req.body.email,
+            phone:req.body.phone,
+            password:hash
+        })
+
+        const result = await newUser.save()
+        res.status(200).json({
+            msg:"account created",
+            data:{
+                fullName:result.fullName,
+                email:result.email,
+                phone:result.phone
+            }
+        })
+
+    }
+    catch(err)
+    {
+        console.log(err)
+        res.status(500).json({
+            error:err
+        })
+    }
 })
 
-// login api
-Router.post('/login',(req,res)=>{
-    res.status(200).json({
-        msg:'login response'
-    })
-})
 
 module.exports = Router
